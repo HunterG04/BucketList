1297a54765553e5428718cbf38e324baa5ecf60e
HunterG04 <grubenhoffhunter@ymail.com>
2018-06-03 22:35:58 -0700

    Added select and command functionality to the DBConnector class

--- BucketList/DBConnector.cs
+++ BucketList/DBConnector.cs
@@ -3,11 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace BucketList
 {
     class DBConnector
     {
+        private string connectionString;
+        private SqlConnection con;
 
+        public void setConnectionString(string newConString)
+        {
+            connectionString = newConString;
+
+            con.ConnectionString = connectionString;
+        }
+
+        public void executeCommand(string cmdQuery)
+        {
+            try
+            {
+                con.Open();
+            }
+            catch (Exception e)
+            {
+                // connection is invalid
+                return;
+            }
+
+            SqlCommand cmd = new SqlCommand(cmdQuery, con);
+
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        public DataSet queryDB(string selectQuery)
+        {
+            try
+            {
+                con.Open();
+            }
+            catch (Exception e)
+            {
+                // connection is invalid
+                return null;
+            }
+
+            SqlCommand cmd = new SqlCommand();
+            SqlDataAdapter adapter = new SqlDataAdapter();
+
+            cmd.CommandText = selectQuery;
+            adapter.SelectCommand = cmd;
+
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+
+            return ds;
+        }
     }
 }

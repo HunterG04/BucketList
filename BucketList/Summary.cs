1ac1c075cac03344e4de85310131ee43d5b939d6
HunterG04 <grubenhoffhunter@ymail.com>
2018-06-05 14:20:19 -0700

    Summary class loads data from the DB in new function

--- BucketList/Summary.cs
+++ BucketList/Summary.cs
@@ -4,6 +4,8 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace BucketList
 {
@@ -12,12 +14,14 @@ namespace BucketList
         private int totalTasks;
         private int numberCompleteTasks;
         private int numberIncompleteTasks;
+        private DBConnector dbCon;
 
-        Summary(int newTotalTasks, int newNumberCompleteTasks, int newNumberIncompleteTasks)
+        Summary(int newTotalTasks, int newNumberCompleteTasks, int newNumberIncompleteTasks, DBConnector newDBCon)
         {
             totalTasks = newTotalTasks;
             numberCompleteTasks = newNumberCompleteTasks;
             numberIncompleteTasks = newNumberIncompleteTasks;
+            dbCon = newDBCon;
         }
 
         public int getTotalTasks()
@@ -65,9 +69,27 @@ namespace BucketList
             numberIncompleteTasks--;
         }
 
-        public void loadSummaryFromDB(SqlConnection con)
+        private void updateDB(string query)
         {
+            dbCon.executeCommand(query);
+        }
 
+        public void loadSummaryFromDB()
+        {
+            string query = "SELECT TOTALTASKS, INCOMPLETETASKS, COMPLETETASKS " +
+                           "FROM SUMMARY ";
+
+            DataSet ds = dbCon.queryDB(query);
+
+            foreach (DataTable table in ds.Tables)
+            {
+                foreach (DataRow row in table.Rows)
+                {
+                    totalTasks = (int)row["TOTALTASKS"];
+                    numberIncompleteTasks = (int)row["INCOMPLETETASKS"];
+                    numberCompleteTasks = (int)row["COMPLETETASKS"];
+                }
+            }
         }
     }
 }
